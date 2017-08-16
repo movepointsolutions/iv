429144b3743209b7078c218df40ce88e2e3eb3e9
Anek Novikov <an146@ya.ru>
2017-08-16 16:15:05 +0300

    parses commands

--- iv.cpp
+++ iv.cpp
@@ -1,6 +1,8 @@
+#include <cctype> /* isprint */
 #include <functional>
 #include <initializer_list>
 #include <map>
+#include <string>
 #include <utility>
 #include <ncurses.h>
 #include <stdio.h>
@@ -23,7 +25,7 @@ public:
 	WINDOW *cmdline() { return cmdline_; }
 	int input() { return wgetch(file()); }
 	void update();
-};
+} window;
 
 Window::Window()
 	: screen_initializer(),
@@ -45,35 +47,85 @@ Window::~Window()
 
 void Window::update()
 {
+	clear();
 	for (WINDOW *w: {file(), status(), cmdline()})
 		wrefresh(w);
 }
 
-struct environment
-{
-	Window window;
-};
+enum class mode_type {
+	NORMAL,
+	INSERT,
+	COMMAND
+} mode;
 
 struct key_bindings
 {
 	typedef std::pair<const int, std::function<void ()>> binding;
 	std::map<binding::first_type, binding::second_type> bindings;
 	key_bindings(const std::initializer_list<binding> &_bindings) : bindings(_bindings) { }
-	void handle(int key) { if (bindings.count(key)) bindings[key](); }
+	bool handle(int key);
 };
+
+bool key_bindings::handle(int key)
+{
+	bool ret = bindings.count(key);
+	if (ret)
+		bindings[key]();
+	return ret;
+}
+
+void handle_command(const std::string &command)
+{
+	std::string x = "[" + command + "]";
+	wprintw(window.file(), x.c_str());
+}
 
 int main(int argc, char **argv)
 {
-	{
-		environment e;
-		key_bindings b({
-			{12, [&e]() { e.window.update(); }}
-		});
-		wprintw(e.window.status(), "Test");
-		while (true) {
-			int c = e.window.input();
-			//printf("%d %s\n", c, key_name(c));
-			b.handle(c);
-		}
+	std::string command;
+
+	key_bindings any_bindings({
+		{12, []() { window.update(); }}
+	});
+
+	key_bindings normal_bindings({
+		{':', [&command]() {
+			mode = mode_type::COMMAND;
+			wclear(window.cmdline());
+			wprintw(window.cmdline(), ":");
+			wrefresh(window.cmdline());
+			command = std::string();
+		}}
+	});
+
+	wprintw(window.file(), "Test");
+	while (true) {
+		int c = window.input();
+		wprintw(window.status(), "%d %s ", c, key_name(c));
+		do {
+			if (any_bindings.handle(c))
+				break;
+			if (mode == mode_type::NORMAL && normal_bindings.handle(c))
+				break;
+			if (mode != mode_type::NORMAL && c == 27) {
+				mode = mode_type::NORMAL;
+				window.update();
+				break;
+			}
+			if (mode == mode_type::COMMAND && std::isprint(c)) {
+				char string[] = {(char)c, '\0'};
+				command.push_back(c);
+				wprintw(window.cmdline(), string);
+				wrefresh(window.cmdline());
+				break;
+			}
+			if (mode == mode_type::COMMAND && c == 10) {
+				mode = mode_type::NORMAL;
+				wclear(window.cmdline());
+				wrefresh(window.cmdline());
+				handle_command(command);
+				break;
+			}
+		} while (false);
 	}
 }
